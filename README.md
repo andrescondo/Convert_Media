830ceb1b249f98f30b71df312252095c6d59f54a
Andres Condo <1dkevinandres.b@gmail.com>
2020-06-14 17:57:06 -0500

    message box added upon conversion completion

---
+++ README.md
@@ -0,0 +1,11 @@
+
+## Si usas linux, y te sale un error donde se te indica que falta un decodificador h.264
+###### instala con el siguiente comando
+> sudo apt-get install h264enc 
+
+###### La libreria correspondiente
+> apt install gstreamer1.0-libav
+
+Para mas información de que hacer, aquí les dejó una página donde explica como hacerlo, si no funciona los comandos anteriores
+[Mas información](https://ubuntu.dokry.com/12526/como-instalar-el-decodificador-h-264.html)
+
--- convert.py
+++ convert.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox #library for message
 from pydub import AudioSegment
 
 #enlace = '/home/andres/Documentos/challenge/python/convert_media/example.mp3'
@@ -16,13 +17,17 @@ class Aplication():
 		self.url = StringVar(value='.mp3')
 
 		#create label and entry
-		self.eti1 = Label(self.window, text='Ingrese el nombre del archivo, con la extensión',bg='#2d2d2d',fg='#fff')
+		self.etiq1 = Label(self.window, text='Ingrese el nombre del archivo, con la extensión',bg='#2d2d2d',fg='#fff')
+
+
 		self.text = Entry(self.window, textvariable=self.url, width=50, bg='beige')
-		self.button1 = Button(self.window, text='Convertir', width=10, height=1, command=self.mp3)
+		self.button1 = Button(self.window, text='Convertir', width=10, height=1, command=self.mp4)
+
+		#bottom exit
 		self.button2 = ttk.Button(self.window, text='Salir', command=self.window.destroy)
 
 		#.pack()  
-		self.eti1.pack(pady=15)
+		self.etiq1.pack(pady=15)
 		self.text.pack(pady=10)
 		self.button1.pack(pady=10)
 		self.button2.pack(side=BOTTOM, pady=10)
@@ -35,6 +40,14 @@ class Aplication():
 
 		song = AudioSegment.from_mp3(url)
 		song.export('./'+url+'.ogg', format='ogg')
+
+	def mp4(self):
+		url=self.url.get()
+
+		ex_video = AudioSegment.from_file(url, 'mp4')
+		ex_video.export('./'+url+'.mp3', format='mp3')
+		messagebox.showinfo('Listo', 'El archivo ya esta convertido').pack()
+
 
 
 
