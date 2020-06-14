2ad38b8826af84ebe6853a0de96fb381c6d7a195
Andres Condo <1dkevinandres.b@gmail.com>
2020-06-14 14:15:10 -0500

    create interface

--- convert.py
+++ convert.py
@@ -1,16 +1,33 @@
+from tkinter import *
+from tkinter import ttk
 from pydub import AudioSegment
 
-def main():
-	print('C O N V E R T I R - A R C H I V O S')
-	url = str(input('Pegue el enlace del archivo a convertir: '))
+# def mp3(url):
+# 	song = AudioSegment.from_mp3(url)
+# 	song.export('./'+url+'.ogg', format='ogg')
 
+
+# def main():
+	# print('C O N V E R T I R - A R C H I V O S')
+	#url = str(input('Escriba el nombre del archivo, con las extension: '))
+	#mp3(url)
 	#enlace = '/home/andres/Documentos/challenge/python/convert_media/'
+#
+def interface():
+	window = Tk()
+	window.geometry('500x300')
+	window.title('Convertidor de archivos')
+	window.configure(bg='#2d2d2d')
+	Label(window, text='Ingrese el nombre del archivo, con la extensión',bg='#2d2d2d',fg='#fff').pack(pady=15)
+	url = StringVar()
+	url_entry = Entry(window, textvariable=url, width=50, bg='beige').pack(pady=10)
+	Button(window, text='Convertir', width=10, height=1).pack(pady=10)
+	ttk.Button(window, text='Salir', command=window.destroy).pack(side=BOTTOM, pady=10)
 
-	song = AudioSegment.from_mp3(url)
-	song.export('/home/andres/Documentos/challenge/python/convert_media/example.ogg', format='ogg')
+	window.mainloop()
 
 
 
 
 if __name__ =='__main__':
-	main()
+	interface()
