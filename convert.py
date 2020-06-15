c1b684f91898e562cbba00bb2307584a2c70d228
Andres Condo <1dkevinandres.b@gmail.com>
2020-06-15 11:04:23 -0500

    added functions for conversions of different formats

--- convert.py
+++ convert.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox #library for message
+# from tkinter.ttk import Progressbar
 from pydub import AudioSegment
 
 #enlace = '/home/andres/Documentos/challenge/python/convert_media/example.mp3'
@@ -9,19 +10,17 @@ from pydub import AudioSegment
 class Aplication():
 	def __init__(self):
 		self.window = Tk()
-		self.window.geometry('500x200')
+		self.window.geometry('600x200')
 		self.window.title('Convertidor de archivos')
 		self.window.configure(bg='#2d2d2d')
 
 		#var
-		self.url = StringVar(value='.mp3')
+		self.url = StringVar()
 
 		#create label and entry
-		self.etiq1 = Label(self.window, text='Ingrese el nombre del archivo, con la extensión',bg='#2d2d2d',fg='#fff')
-
-
-		self.text = Entry(self.window, textvariable=self.url, width=50, bg='beige')
-		self.button1 = Button(self.window, text='Convertir', width=10, height=1, command=self.mp4)
+		self.etiq1 = Label(self.window, text='Ingrese el nombre del archivo, con la extensión',bg='#2d2d2d',fg='#fff', font=("Arial",16))
+		self.text = Entry(self.window, textvariable=self.url, width=70, bg='beige')
+		self.button1 = Button(self.window, text='Convertir', width=10, height=1, command=self.mp4_wav)
 
 		#bottom exit
 		self.button2 = ttk.Button(self.window, text='Salir', command=self.window.destroy)
@@ -34,19 +33,65 @@ class Aplication():
 
 		self.window.mainloop()
 
-
-	def mp3(self):
+#======================== AUDIO ============================================
+	def mp3_ogg(self):
 		url = self.url.get()
-
 		song = AudioSegment.from_mp3(url)
 		song.export('./'+url+'.ogg', format='ogg')
-
-	def mp4(self):
-		url=self.url.get()
-
-		ex_video = AudioSegment.from_file(url, 'mp4')
-		ex_video.export('./'+url+'.mp3', format='mp3')
 		messagebox.showinfo('Listo', 'El archivo ya esta convertido').pack()
+
+	def ogg_mp3(self):
+		url = self.url.get()
+		song = AudioSegment.from_ogg(url)
+		song.export('./'+url+'.mp3', format='.mp3')
+		messagebox.showinfo('Listo', 'El archivo ya esta convertido').pack()
+
+	def mp3_wav(self):
+		url = self.url.get()
+		song = AudioSegment.from_wav(url)
+		song.export('./'+url+'.wav', format='.wav')
+		messagebox.showinfo('Listo', 'El archivo ya esta convertido').pack()
+
+	def wav_mp3(self):
+		url = self.url.get()
+		song = AudioSegment.from_mp3(url)
+		song.export('./'+url+'.mp3', format='.mp3')
+		messagebox.showinfo('Listo', 'El archivo ya esta convertido').pack()
+
+
+#========================= VIDEOS =======================================
+	def mp4_mp3(self):
+		url=self.url.get()
+		video = AudioSegment.from_file(url, 'mp4')
+		video.export('./'+url+'.mp3', format='mp3')
+		messagebox.showinfo('Listo', 'El archivo ya esta convertido').pack()
+
+	def mp4_wav(self):
+		url=self.url.get()
+		video = AudioSegment.from_file(url, 'mp4')
+		video.export('./'+url+'.wav', format='wav')
+		messagebox.showinfo('Listo', 'El archivo ya esta convertido').pack()
+
+	def mp4_flv(self):
+		url=self.url.get()
+		video = AudioSegment.from_file(url, 'mp4')
+		video.export('./'+url+'.flv', format='flv')
+		messagebox.showinfo('Listo', 'El archivo ya esta convertido').pack()
+
+	def flv_mp4(self):
+		url=self.url.get()
+		video = AudioSegment.from_file(url, 'flv')
+		video.export('./'+url+'.mp4', format='mp4')
+		messagebox.showinfo('Listo', 'El archivo ya esta convertido').pack()
+
+	def flv_mp3(self):
+		url=self.url.get()
+		video = AudioSegment.from_file(url, 'flv')
+		video.export('./'+url+'.mp3', format='mp3')
+		messagebox.showinfo('Listo', 'El archivo ya esta convertido').pack()
+	
+
+
 
 
 
