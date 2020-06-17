a9b3f24d7ab126df34650cea09b13f95fe49e924
Andres Condo <1dkevinandres.b@gmail.com>
2020-06-17 16:19:14 -0500

    added video conversions to the interface, and scroll bar removal

--- convert.py
+++ convert.py
@@ -5,9 +5,9 @@ from tkinter import ttk
 from tkinter import messagebox #library for message
 from pydub import AudioSegment
 
-#enlace = '/home/andres/Documentos/challenge/python/convert_media/example.mp3'
+# Si estas revisando los commit que sepas que esto es nuevo para mi, es la primera vez que hago un proyecto asi
+# espero que te guste el programa y le saques provecho
 
-#
 class Aplication():
 	def __init__(self):
 		self.window = Tk()
@@ -16,18 +16,17 @@ class Aplication():
 		self.window.configure(bg='#2d2d2d')#, cursor='heart'		
 
 		#var
-		self.url = StringVar(value='.extensión')
-		self.mp4_3 = StringVar(value='.extensión')
-		self.mp4_w = StringVar(value='.extensión')
-		self.mp4_f = StringVar(value='.extensión')
-		self.flv_4 = StringVar(value='.extensión')
-		self.mp3_w = StringVar(value='.extensión')
-		self.mp3_o = StringVar(value='.extensión')
+		self.video_3 = StringVar(value='NOMBRE.extensión')
+		self.mp4_f = StringVar(value='NOMBRE.extensión')
+		self.flv_4 = StringVar(value='NOMBRE.extensión')
+		self.audio_3 = StringVar(value='NOMBRE.extensión')
+		self.mp3_o = StringVar(value='NOMBRE.extensión')
+		self.mp3_w = StringVar(value='NOMBRE.extensión')
 
 
 		#create label and entry
-		self.etiq1=Label(self.window,text='Ingrese el nombre del archivo, o la ruta donde se encuentra',bg='#2d2d2d',fg='#fff',font=("Arial",16))
-		# color celeste #b5deda
+		self.etiq1=Label(self.window,text='NOMBRE DEL ARCHIVO, O LA RUTA DONDE SE ENCUENTRA',bg='#2d2d2d',fg='#fff',font=("Arial",15))
+		# color celeste #b5deda  
 		#==================
 		self.tab_control = ttk.Notebook(self.window)
 
@@ -35,32 +34,46 @@ class Aplication():
 		self.tab1 = ttk.Frame(self.tab_control)
 		self.tab_control.add(self.tab1, text=' VIDEOS ')
 
-		#mp4 a mp3
 		self.labl1_1 = Label(self.tab1, text='Convertir de MP4 o FLV, a MP3',bg='#2d2d2d',fg='#fff',font=("Arial",14) )
-		self.text1_1 = Entry(self.tab1, textvariable=self.mp4_3, width=80, bg='beige')
+		self.text1_1 = Entry(self.tab1, textvariable=self.video_3, width=80, bg='beige')
 		self.button1_1 = Button(self.tab1, text='Convertir', width=10, height=1, command=self.video_mp3)
 
+		self.labl1_2 = Label(self.tab1, text='Convertir de MP4 a LFV',bg='#2d2d2d',fg='#fff',font=("Arial",14) )
+		self.text1_2 = Entry(self.tab1, textvariable=self.mp4_f, width=80, bg='beige')
+		self.button1_2 = Button(self.tab1, text='Convertir', width=10, height=1, command=self.mp4_flv)
 
+		self.labl1_3 = Label(self.tab1, text='Convertir de FLV a MP4',bg='#2d2d2d',fg='#fff',font=("Arial",14) )
+		self.text1_3 = Entry(self.tab1, textvariable=self.flv_4, width=80, bg='beige')
+		self.button1_3 = Button(self.tab1, text='Convertir', width=10, height=1, command=self.flv_mp4)
 
 		#====================== box AUDIO ==================================
 		self.tab2 = ttk.Frame(self.tab_control)
 		self.tab_control.add(self.tab2, text=' AUDIO ')
-
 		#mp4 a flv
 		self.labl2_1 = Label(self.tab2, text='Convertir de MP3 a OGG',bg='#2d2d2d',fg='#fff',font=("Arial",14) )
-		self.text2_1 = Entry(self.tab2, textvariable=self.mp3_o, width=80, bg='beige')
-		self.button2_1 = Button(self.tab2, text='Convertir', width=10, height=1, command=self.mp3_ogg)
+		self.text2_1 = Entry(self.tab2, textvariable=self.audio_3, width=80, bg='beige')
+		self.button2_1 = Button(self.tab2, text='Convertir', width=10, height=1, command=self.audio_mp3)
 
 		#bottom exit
 		self.button_exit = ttk.Button(self.window, text='Salir', command=self.window.destroy)
 
-		#====.pack()  
+		#====.pack()=====================================  
 		self.etiq1.pack(pady=15)
 
+		#=== 1_# VIDEO
 		self.labl1_1.pack(pady=10)
 		self.text1_1.pack(pady=10)
 		self.button1_1.pack(pady=10)
+		#--------
+		self.labl1_2.pack(pady=10)
+		self.text1_2.pack(pady=10)
+		self.button1_2.pack(pady=10)
+		#--------
+		self.labl1_3.pack(pady=10)
+		self.text1_3.pack(pady=10)
+		self.button1_3.pack(pady=10)
 
+		#=== 2_# AUDIO
 		self.labl2_1.pack(pady=10)
 		self.text2_1.pack(pady=10)
 		self.button2_1.pack(pady=10)
@@ -80,10 +93,12 @@ class Aplication():
 		song.export('./'+url+'.ogg', format='ogg')
 		messagebox.showinfo('Listo', 'El archivo ya esta convertido').pack()
 
-	def ogg_mp3(self):
-		url = self.ogg_3.get()
-		song = AudioSegment.from_ogg(url)
-		song.export('./'+url+'.mp3', format='mp3')
+	def audio_mp3(self):
+		url = self.audio_3.get()
+		extension_list = ('*.wav','*.ogg')
+		for extension in extension_list:
+			mp3_filename = os.path.splitext(url)[0] + '.mp3'
+			AudioSegment.from_file(url).export(mp3_filename, format='mp3')
 		messagebox.showinfo('Listo', 'El archivo ya esta convertido').pack()
 
 	def mp3_wav(self):
@@ -92,17 +107,16 @@ class Aplication():
 		song.export('./'+url+'.wav', format='wav')
 		messagebox.showinfo('Listo', 'El archivo ya esta convertido').pack()
 
-	def wav_mp3(self):
-		url = self.wav_3.get()
-		song = AudioSegment.from_file(url)
-		song.export('./'+url+'.mp3', format='mp3')
-		messagebox.showinfo('Listo', 'El archivo ya esta convertido').pack()
-
+	# def wav_mp3(self):
+	# 	url = self.wav_3.get()
+	# 	song = AudioSegment.from_file(url)
+	# 	song.export('./'+url+'.mp3', format='mp3')
+	# 	messagebox.showinfo('Listo', 'El archivo ya esta convertido').pack()
 
 #========================= VIDEOS =======================================
 	def video_mp3(self):
-		url=self.mp4_3.get()
-		extension_list = ('*.mp4', '*.flv','*.wav','*.ogg')
+		url=self.video_3.get()
+		extension_list = ('*.mp4', '*.flv')
 
 		for extension in extension_list:
 			mp3_filename = os.path.splitext(url)[0] + '.mp3'
@@ -111,14 +125,14 @@ class Aplication():
 
 	def mp4_flv(self):
 		url=self.mp4_f.get()
-		video = AudioSegment.from_file(url, 'mp4')
-		video.export('./'+url+'.flv', format='flv')
+		flv_filename = os.path.splitext(url)[0] + '.flv'
+		AudioSegment.from_file(url).export(flv_filename, format='flv')
 		messagebox.showinfo('Listo', 'El archivo ya esta convertido').pack()
 
 	def flv_mp4(self):
 		url=self.flv_4.get()
-		video = AudioSegment.from_file(url, 'flv')
-		video.export('./'+url+'.mp4', format='mp4')
+		mp4_filename = os.path.splitext(url)[0] + '.mp4'
+		AudioSegment.from_file(url).export(mp4_filename, format='mp4')
 		messagebox.showinfo('Listo', 'El archivo ya esta convertido').pack()
 
 
