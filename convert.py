f3bc4e801fb63157ccaa6440ac3528163fb5b13c
Andres Condo <1dkevinandres.b@gmail.com>
2020-06-25 12:46:22 -0500

    finished the development in the edition section and updated design and logic when converting to audio formats

--- convert.py
+++ convert.py
@@ -15,12 +15,8 @@ class Aplication():
 		self.window['bg'] = '#2d2d2d'
 
 		#var
-		self.video_3 = StringVar(value='NOMBRE.extensión')
-		self.mp4_f = StringVar(value='NOMBRE.extensión')
-		self.flv_4 = StringVar(value='NOMBRE.extensión')
-		self.audio_3 = StringVar(value='NOMBRE.extensión')
-		self.mp3_o = StringVar(value='NOMBRE.extensión')
-		self.mp3_w = StringVar(value='NOMBRE.extensión')
+		self.convert_3 = StringVar(value='NOMBRE.extensión')
+		self.convert_w = StringVar(value='NOMBRE.extensión')
 		self.edit = StringVar(value='NOMBRE.extensión')
 		self.clase = StringVar(value='i')
 		self.seg_record = DoubleVar(value=10)
@@ -38,19 +34,19 @@ class Aplication():
 		self.tab_control.add(self.tab1, text=' CONVERTIR ')
 		
 		self.labl1_1 = Label(self.tab1, text='Convertir a MP3',bg='#2d2d2d',fg='#fff',font=("Arial,Bold",14) )
-		self.text1_1 = Entry(self.tab1, textvariable=self.audio_3, width=80, bg='beige')
-		self.button1_1 = Button(self.tab1, text='Convertir', width=10, height=1,bg='#A7A8A9', command=self.audio_mp3)
+		self.text1_1 = Entry(self.tab1, textvariable=self.convert_3, width=80, bg='beige')
+		self.button1_1 = Button(self.tab1, text='Convertir', width=10, height=1,bg='#A7A8A9', command=self.convert_mp3)
 
 		self.labl1_3 = Label(self.tab1, text='Convertir a WAV',bg='#2d2d2d',fg='#fff',font=("Arial,Bold",14) )
-		self.text1_3 = Entry(self.tab1, textvariable=self.mp3_w, width=80, bg='beige')
-		self.button1_3 = Button(self.tab1, text='Convertir', width=10, height=1,bg='#A7A8A9', command=self.mp3_wav)
+		self.text1_3 = Entry(self.tab1, textvariable=self.convert_w, width=80, bg='beige')
+		self.button1_3 = Button(self.tab1, text='Convertir', width=10, height=1,bg='#A7A8A9', command=self.convert_wav)
 
 		self.message = Label(self.tab1, text='Se pueden convertir los siguientes formatos: mp4, flv, ogg, mp3, wav',bg='#2d2d2d',fg='#fff')
 
 		#====================== Edition ====================================
 		self.tab2 = ttk.Frame(self.tab_control)
 		self.tab_control.add(self.tab2, text=' RECORTAR ')
-		self.labl2_1 = Label(self.tab2, text='Ingrese el Audio que desea editar',bg='#2d2d2d',fg='#fff',font=("Arial,Bold",14) )
+		self.labl2_1 = Label(self.tab2, text='Ingrese el archivo que desea editar',bg='#2d2d2d',fg='#fff',font=("Arial,Bold",14) )
 		self.text2_2 = Entry(self.tab2, textvariable=self.edit, width=80, bg='beige')
 		self.labl2_3 = Label(self.tab2, text='Ingrese cuantos segundos desea recortar',bg='#2d2d2d',fg='#fff',font=("Arial,Bold",10))
 		self.text2_4 = Entry(self.tab2, textvariable=self.seg_record, width=10, bg='beige')
@@ -79,6 +75,7 @@ class Aplication():
 		self.labl1_3.pack(padx=40,pady=10, anchor='nw')
 		self.text1_3.pack(pady=10)
 		self.button1_3.pack(pady=10)
+
 		self.message.pack(padx=30, pady=5, anchor='se')
 
 		#=== 2_# EDICION
@@ -98,23 +95,12 @@ class Aplication():
 		self.tab_control.pack(expand=1, fill='both')
 
 
-		#self.window.config(menu=self.menu)
 		self.window.mainloop()
 
 #======================== AUDIO ============================================
-	def mp3_ogg(self):
+	def convert_mp3(self):
 		try:
-			url = self.mp3_o.get()
-			ogg_filename = os.path.splitext(url)[0] + '.ogg'
-			AudioSegment.from_file(url).export(ogg_filename, format='ogg')
-		except FileNotFoundError:
-			messagebox.showinfo('Fallo', 'El archivo que pusiste no existe, o el nombre esta incorrecto').pack()
-
-		messagebox.showinfo('Listo', 'El archivo ya esta convertido').pack()
-
-	def audio_mp3(self):
-		try:
-			url = self.audio_3.get()
+			url = self.convert_3.get()
 			extension_list = ('*.wav','*.ogg')
 			for extension in extension_list:
 				mp3_filename = os.path.splitext(url)[0] + '.mp3'
@@ -124,25 +110,11 @@ class Aplication():
 			
 		messagebox.showinfo('Listo', 'El archivo ya esta convertido').pack()
 
-	def mp3_wav(self):
+	def convert_wav(self):
 		try:
-			url = self.mp3_w.get()
+			url = self.convert_w.get()
 			wav_filename = os.path.splitext(url)[0] + '.wav'
 			AudioSegment.from_file(url).export(wav_filename, format='wav')
-		except FileNotFoundError:
-			messagebox.showinfo('Fallo', 'El archivo que pusiste no existe, o el nombre esta incorrecto').pack()
-
-		messagebox.showinfo('Listo', 'El archivo ya esta convertido').pack()
-
-
-#========================= VIDEOS =======================================
-	def video_mp3(self):
-		try:
-			url=self.video_3.get()
-			extension_list = ('*.mp4', '*.flv')
-			for extension in extension_list:
-				mp3_filename = os.path.splitext(url)[0] + '.mp3'
-				AudioSegment.from_file(url).export(mp3_filename, format='mp3')
 		except FileNotFoundError:
 			messagebox.showinfo('Fallo', 'El archivo que pusiste no existe, o el nombre esta incorrecto').pack()
 
